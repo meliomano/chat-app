c93ddfb6342ec67d085bd281917922528ca79f61
Elio Perez <vulkanorz@gmail.com>
2020-06-16 20:32:58 +0200

    Broadcast clients' messages and ACKs delivered from server

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http')
 const path = require('path')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const port = process.env.PORT || 3000
 
@@ -13,9 +14,31 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log('New WebSocket connection')
+    socket.emit('message', 'Welcome!')
+
+    socket.broadcast.emit('message', 'A new user has joined!')
+
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+        io.emit('message', message)
+        callback()
+    })
+
+    socket.on('sendLocation', ({ latitude, longitude }, callback ) => {
+        io.emit('message', `https://google.com/maps?q=${longitude},${latitude}`)
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
+    })
 })
+
 
 server.listen(port, () => {
     console.log(`Server is up on port ${port}!`)
